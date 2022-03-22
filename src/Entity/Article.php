3e079a2abd4b5cc0b88d732b4dd962ba3d254520
BesokDev <74706532+BesokDev@users.noreply.github.com>
2022-03-22 19:19:04 +0100

    Heroku

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -6,6 +6,7 @@ use App\Repository\ArticleRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -71,6 +72,11 @@ class Article
      * @ORM\OneToMany(targetEntity=Commentary::class, mappedBy="article")
      */
     private $commentaries;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="articles")
+     */
+    private $author;
 
     public function __construct()
     {
@@ -216,6 +222,18 @@ class Article
                 $commentary->setArticle(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?UserInterface $author): self
+    {
+        $this->author = $author;
 
         return $this;
     }
--- src/Entity/Commentary.php
+++ src/Entity/Commentary.php
@@ -39,6 +39,11 @@ class Commentary
      */
     private $article;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="commentaries")
+     */
+    private $author;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -64,6 +69,18 @@ class Commentary
     public function setArticle(?Article $article): self
     {
         $this->article = $article;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): self
+    {
+        $this->author = $author;
 
         return $this;
     }
